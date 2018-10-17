a50aa98d2e5e8775d6dc1c2e92f0c8a5390f6b16
Abhinav Patil <apatil.cs@gmail.com>
2018-10-16 20:10:45 -0400

    Finished through exercise 6.09

--- src/main/scala/exercises/chapter06/RNG.scala
+++ src/main/scala/exercises/chapter06/RNG.scala
@@ -126,8 +126,78 @@ object RNG {
     */
   def ints2(count: Int)(rng: RNG): Rand[List[Int]] = sequence(List.fill(count)(int))
 
+  /**
+    * Exercise 6.8: Implement `flatMap`
+    */
+  def flatMap[A,B](f: Rand[A])(g: A => Rand[B]): Rand[B] = rng => {
+    val (a, rng2) = f(rng)
+    g(a)(rng2)
+  }
+
+  /**
+    * Exercise 6.8 (cont'd): Implement `nonNegativeLessThan` using `flatMap`
+    */
+  def nonNegativeLessThan(n: Int): Rand[Int] =
+    flatMap(nonNegativeInt) {
+      i => {
+        val mod = i % n
+        if (i + (n-1) - mod >= 0)
+          unit(mod)
+        else
+          nonNegativeLessThan(n)
+      }
+    }
+
+  /**
+    * Exercise 6.9: Reimplement `map` in terms of flatMap.
+    */
+  def mapViaFlatMap[A,B](s: Rand[A])(f: A => B): Rand[B] = flatMap(s)(a => unit(f(a)))
+
+  /**
+    * Exercise 6.9 (cont'd): Reimplement `map2` in terms of flatMap.
+    */
+  def map2ViaFlatMap[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
+    flatMap(ra)(a => mapViaFlatMap(rb)(b => f(a, b)))
+
   def main(args: Array[String]): Unit = {
 
   }
 
+}
+
+import State._
+
+/**
+  * Exercise 6.10: Generalize the functions unit, map, map2, flatMap, and sequence. Add them as methods on the State
+  * case class where possible.
+  */
+case class State[S, +A](run: S => (A, S)) {
+  def map[B](f: A => B): State[S, B] = State((s: S) => {
+    val (a, rng2) = run(s)
+    (f(a), rng2)
+  })
+
+  def map2[B,C](s2: State[S, B])(f: (A, B) => C): State[S, C] = State(s => {
+    val (a, s3) = run(s)
+    val (b, s4) = s2.run(s3)
+    (f(a, b), s4)
+  })
+
+  def flatMap[B](f: A => State[S, B]): State[S, B] = State(s1 => {
+    val (a, s2) = run(s1)
+    f(a).run(s2)
+  })
+
+  def mapViaFlatMap[B](f: A => B): State[S, B] =
+    flatMap(a => unit(f(a)))
+
+  def map2ViaFlatMap[B,C](s2: State[S, B])(f: (A, B) => C): State[S, C] = flatMap(a => s2.mapViaFlatMap(b => f(a, b)))
+}
+
+/**
+  * Exercise 6.10 (cont'd): Generalize the functions unit, map, map2, flatMap, and sequence. Add them as methods on the
+  * State case class where possible.
+  */
+object State {
+  def unit[S, A](a: A): State[S, A] = State(s => (a, s))
 }
